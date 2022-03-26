695bdf2202072b44058d05da31f37af253b0da66
Sunny Gupta <sunny.gupta@globallogic.com>
2022-03-26 22:11:45 +0530

    txt update

--- Readme.md
+++ Readme.md
@@ -1,2 +1,9 @@
+npm init
+
 To run app
 node app.js
+
+node db-demo/mongo-test-queries.js 
+
+Todo
+read/write file
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+console.log("Hello World!");
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,9 +8,41 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World     v');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });   // Send the HTTP header, HTTP Status: 200 : OK, Content Type: text/plain
+  console.log('got http://127.0.0.1:8081/');
+  res.write('hi');
+  res.end('Hello World\n');  // Send the response body as "Hello World"
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  
 });
+
+// Import events module
+var events = require('events');
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+   console.log('data received succesfully.');
+});
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
+
